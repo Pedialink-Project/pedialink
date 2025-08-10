c40f05233c4c7c30330f150791bcb78b4fb165f5
mani-hash <manimehalan400@gmail.com>
2025-08-10 19:03:25 +0530

    feat: button component [TASK-18]

---
+++ resources/views/components/button.view.php
@@ -0,0 +1,32 @@
+<?php
+$classes = 'btn';
+
+if (!empty($class)) {
+    $classes .= ' ' . $class;
+}
+
+$type = $type ?? '';
+if ($type === 'primary') {
+    $classes .= ' btn-primary';
+} else if ($type === 'ghost') {
+    $classes .= ' btn-ghost';
+} else if ($type === 'outline'){
+    $classes .= ' btn-outline';
+} else if ($type === 'subtle') {
+    $classes .= ' btn-subtle';
+}
+
+$size = $size ?? '';
+if ($size === 'sm') {
+    $classes .= ' btn-sm';
+} else if ($size === 'lg') {
+    $classes .= ' btn-lg';
+}
+
+// if icon-only
+if (!empty($icon_only)) $classes .= ' btn-icon';
+?>
+
+<button class="{{ $classes }}">
+  {{ $slot }}
+</button>
