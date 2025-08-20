719c51f861e8c1fa241a72e6b56b4d4d23cdf1a9
mani-hash <manimehalan400@gmail.com>
2025-08-20 19:47:35 +0530

    feat: auth routes [TASK-71]

--- routes/sectors/auth.php
+++ routes/sectors/auth.php
@@ -3,8 +3,8 @@
 use App\Controllers\AuthController;
 
 return [
-    ['GET', '/parent/register', [AuthController::class, 'parentRegister'], 'parent.register'],
-    ['GET', '/parent/login', [AuthController::class, 'parentLogin'], 'parent.login'],
-    ['GET', '/staff/login', [AuthController::class, 'staffLogin'], 'staff.login'],
+    ['GET', '/parent/register', [AuthController::class, 'parentRegister'], 'parent.register', ['guest']],
+    ['GET', '/parent/login', [AuthController::class, 'parentLogin'], 'parent.login', ['guest']],
+    ['GET', '/staff/login', [AuthController::class, 'staffLogin'], 'staff.login', ['guest']],
 
 ];
