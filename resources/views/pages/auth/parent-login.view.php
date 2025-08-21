7307f33afe5a809b8b12727d29d49b1bbf631206
mani-hash <manimehalan400@gmail.com>
2025-08-21 12:01:56 +0530

    feat: added route links [TASK-71]

--- resources/views/pages/auth/parent-login.view.php
+++ resources/views/pages/auth/parent-login.view.php
@@ -5,17 +5,18 @@
 @endsection
 
 @section('css')
+    <link rel="stylesheet" href="{{ asset('css/pages/auth/common.css') }}">
     <link rel="stylesheet" href="{{ asset('css/pages/auth/parent-login.css') }}">
 @endsection
 
 @section('content')
     <div id="main">
         <div>
-            <c-link class="return-link-group">
+            <c-link href="{{ route('home') }}" class="return-link-group">
                 <c-slot name="icon">
                     <svg width="25px" height="25px" viewBox="0 0 1024 1024" fill="#000000" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z" fill="" /></svg>
                 </c-slot>
-                Go Back
+                Go to Home
             </c-link>
         </div>
         <div class="form-container">
@@ -25,20 +26,20 @@
             </div>
             <c-card class="form-content">
                 <div class="tab-links">
-                    <c-link type="subtle" class="active">
+                    <c-link href="{{ route('parent.login') }}" type="subtle" class="active">
                         Parent
                     </c-link>
-                    <c-link type="subtle">
+                    <c-link href="{{ route('staff.login') }}" type="subtle">
                         Staff
                     </c-link>
                 </div>
                 <div>
                     <form id="parent-login" action="">
-                        <c-input label="Email:" type="text" />
-                        <c-input label="Password:" type="password" />
+                        <c-input label="Email:" type="text" placeholder="Enter your email" />
+                        <c-input label="Password:" type="password" placeholder="Enter your password" />
                     </form>
                     <div class="forgot-password-group">
-                        <c-link size="sm">
+                        <c-link href="{{ route('forgot.password') }}" size="sm">
                             Forgot Password?
                         </c-link>
                     </div>
@@ -49,7 +50,7 @@
                     </c-button>
                     <div class="sign-up-group">
                         <span class="text-muted">Don't have an account?</span>
-                        <c-link>Sign up</c-link>
+                        <c-link href="{{ route('parent.register') }}">Sign up</c-link>
                     </div>
                 </div>
             </c-card>
