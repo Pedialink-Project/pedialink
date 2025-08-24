c8a7ac58a3f5aea08e7c69b17636d591cef4ca21
mani-hash <manimehalan400@gmail.com>
2025-08-24 13:34:08 +0530

    feat: routes for login post requests [TASK-71]

--- routes/sectors/auth.php
+++ routes/sectors/auth.php
@@ -8,7 +8,9 @@ return [
     ['GET', '/parent/register/final', [AuthController::class, 'parentRegisterFinal'], 'parent.register.final', ['guest']],
     ['POST', '/parent/register/final', [AuthController::class, 'parentVerifyFinal'], 'parent.register.final.submit', ['guest']],
     ['GET', '/parent/login', [AuthController::class, 'parentLogin'], 'parent.login', ['guest']],
+    ['POST', '/parent/login', [AuthController::class, 'loginAsParent'], 'parent.login.submit', ['guest']],
     ['GET', '/staff/login', [AuthController::class, 'staffLogin'], 'staff.login', ['guest']],
+    ['POST', '/staff/login', [AuthController::class, 'loginAsStaff'], 'staff.login.submit', ['guest']],
     ['GET', '/forgot-password', [AuthController::class, 'forgotPassword'], 'forgot.password', ['guest']],
     ['POST', '/logout', [AuthController::class, 'logout'], 'logout', ['auth']]
 ];
