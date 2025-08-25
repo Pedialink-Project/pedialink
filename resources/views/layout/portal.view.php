404245b78df925cd1f3c15ff83512ca5e4318aff
Keeththi2003 <keeththi2003@gmail.com>
2025-08-26 02:19:59 +0530

    feat: Sidebar component fully added [TASK-71]

--- resources/views/layout/portal.view.php
+++ resources/views/layout/portal.view.php
@@ -1,11 +1,23 @@
-<div class="app-wrapper" style="display:flex;">
-    <c-sidebar $type="<?= $_SESSION['role'] ?? 'guest' ?>" $class="my-sidebar">
-        <c-slot name="header"><h3>Menu</h3></c-slot>
-        <c-slot name="footer"><p>Help & Support</p></c-slot>
-    </c-sidebar>
+<!doctype html>
+<html lang="en">
 
-    <div id="main-content" style="flex:1; padding:20px;">
-        @yield('content')
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="css/app.css">
+    @yield('css')
+</head>
+
+<body>
+    <div style="display:flex;">
+        <div>
+             @yield('sidebar')
+        </div>
+        <div style="flex:1;">
+            @yield('content')
+        </div>
     </div>
-</div>
+    @yield('scripts')
+</body>
 
+</html>
