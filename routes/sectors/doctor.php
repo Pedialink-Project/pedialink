6670b7eb4608d2b5f1303ec575946d45c42877c3
mani-hash <manimehalan400@gmail.com>
2025-10-21 01:12:27 +0530

    fix: doctor setting and notification route [TASK-92]

--- routes/sectors/doctor.php
+++ routes/sectors/doctor.php
@@ -6,7 +6,8 @@ use App\Controllers\Doctor\ChildProfileController;
 use App\Controllers\Doctor\MaternalProfileController;
 use App\Controllers\Doctor\MaternalHealthController;
 use App\Controllers\Doctor\AppointmentController;
-use App\Controllers\TestController;
+use App\Controllers\NotificationController;
+use App\Controllers\SettingController;
 
 return [
     ['GET', '/doctor/dashboard', [DashboardController::class, 'index'], 'doctor.dashboard', ['doctor']],
@@ -16,6 +17,6 @@ return [
     ['GET', '/doctor/maternal-profiles', [MaternalProfileController::class, 'index'], 'doctor.maternal.profiles', ['doctor']],
     ['GET', '/doctor/maternal-profiles/{id}/health-records', [MaternalHealthController::class, 'index'], 'doctor.maternal.health', ['doctor']],
     ['GET', '/doctor/appointments', [AppointmentController::class, 'index'], 'doctor.appointments', ['doctor']],
-    ['GET', '/doctor/notification', [TestController::class, 'index'], 'doctor.notification', ['doctor']],
-    ['GET', '/doctor/settings', [TestController::class, 'index'], 'doctor.settings', ['doctor']],
+    ['GET', '/doctor/notification', [NotificationController::class, 'index'], 'doctor.notification', ['doctor']],
+    ['GET', '/doctor/settings', [SettingController::class, 'index'], 'doctor.settings', ['doctor']],
 ];
