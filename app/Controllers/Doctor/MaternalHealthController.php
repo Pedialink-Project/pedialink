89585af00ad342b8d565b48641ddb203abd355d8
Keeththi2003 <keeththi2003@gmail.com>
2025-10-22 00:29:19 +0530

    feat: update MaternalHealthController to retrieve maternal statistics by maternal ID [TASK-119]

--- app/Controllers/Doctor/MaternalHealthController.php
+++ app/Controllers/Doctor/MaternalHealthController.php
@@ -1,15 +1,25 @@
 <?php
 
 namespace App\Controllers\Doctor;
+use App\Services\MaternalStatService;
 
 use Library\Framework\Http\Request;
 
 class MaternalHealthController
 {
+    protected $MaternalStatService;
+
+    public function __construct()
+    {
+        $this->MaternalStatService = new MaternalStatService();
+    }
+
     public function index(Request $request, int $id)
     {
+        
+        $maternalStats = $this->MaternalStatService->getMaternalStatByMaternalId($id);
         return view("doctor/maternalhealth", [
-            "id" => $id,
+            "items"=>$maternalStats
         ]);
     }
 }
