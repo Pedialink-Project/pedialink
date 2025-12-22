bdbcc87b570feaf0346958edf0ee92e724c5602a
Keeththi2003 <keeththi2003@gmail.com>
2025-12-22 13:34:50 +0530

    feat: implement EventService with method to retrieve all events [TASK-132]

---
+++ app/Services/EventService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Events;
+
+class EventService
+{
+    private function getAllEvents(): array
+    {
+        $events = Events::all();
+
+        $resource = [];
+
+        foreach ($events as $event) {
+            $resource[] = [
+                'id' => $event->id,
+                'title' => $event->title,
+                'description' => $event->description,
+                'purpose' => $event->purpose,
+                'notes' => $event->notes,
+                'event_date' => $event->event_date,
+                'event_time' => $event->event_time,
+                'event_location' => $event->event_location,
+                'max_count' => $event->max_count,
+                'admin' => $event->getAdmin() ? [
+                    'id' => $event->getAdmin()->id,
+                    'name' => $event->getAdmin()->name,
+                    'email' => $event->getAdmin()->email,
+                ] : null,
+            ];
+        }
+
+        return $resource;
+    }
+
+}
+?>
