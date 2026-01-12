d56ac5be61b89a858fbf65136e042c56ef07d827
mani-hash <manimehalan400@gmail.com>
2026-01-12 09:52:47 +0530

    feat: add email verification after parent registration [TASK-135]

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,15 +4,18 @@ namespace App\Controllers;
 
 use App\Models\Area;
 use App\Services\AuthService;
+use App\Services\EmailVerificationService;
 use Library\Framework\Http\Request;
 
 class AuthController
 {
     private AuthService $authService;
+    private EmailVerificationService $emailVerificationService;
 
     public function __construct()
     {
         $this->authService = new AuthService();
+        $this->emailVerificationService = new EmailVerificationService();
     }
 
     public function parentRegisterInitial(Request $request)
@@ -85,6 +88,21 @@ class AuthController
             $data = array_merge($data, session()->get("register_form"));
 
             $user = $this->authService->createParentAccount($data);
+
+            $verifyLink = $this->emailVerificationService
+                    ->createVerificationUrl(
+                        $user, config('app.key')
+                    );
+
+            mailer()->sendTemplate(
+                $user->email,
+                'verify-email',
+                [
+                    'username' => $user->name,
+                    'verify_link' => $verifyLink
+                ],
+                'Verify your email',
+            );
 
             auth()->login($user);
 
