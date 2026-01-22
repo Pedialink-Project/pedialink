d26f5fe7e769c83630eaa8f43ba12b5f376a656e
mani-hash <manimehalan400@gmail.com>
2026-01-22 23:30:46 +0530

    feat: child access control class methods [TASK-140]

--- app/Controllers/Admin/ChildController.php
+++ app/Controllers/Admin/ChildController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers\Admin;
 
+use App\Models\Child;
 use App\Services\Admin\ChildService;
 use Library\Framework\Http\Request;
 
@@ -26,10 +27,44 @@ class ChildController
 
     public function accessControl(Request $request, int $id)
     {
+        $child = Child::find((int)$id);
+        if (!$child) {
+            return view("error/404");
+        }
+
+        $accessData = $this->childService->getAccessControlData($child);
+
         return view("admin/child/control", [
             "id" => $id,
-            "name" => "Nancy Jenkins"
+            "name" => $child->name,
+            "primaryAccess" => $accessData["parents"] ?? [],
+            "secondaryAccess" => $accessData["phm"] ?? []
         ]);
+    }
+
+    public function removeLinkage(Request $request, int $id)
+    {
+        $child = Child::find($id);
+        $data = [
+            "id" => $request->input("id") ?? "",
+            "type" => $request->input("type") ?? ""
+        ];
+
+        if ($child) {
+            if ($data["type"] === "phm") {
+                $child->phm_id = null;
+                $child->save();
+            } else if ($data["type"] === "parent") {
+                $child->parent_id = null;
+                $child->save();
+            }
+
+            return redirect(route("admin.child.access.control", ["id" => $id]))
+                ->withMessage("Successfully removed link", "Success", "success");
+        }
+
+        return redirect(route("admin.child.access.control", ["id" => $id]))
+            ->withMessage("Failed to removed link", "Fail", "error");
     }
 
     public function linkageRequests(Request $request)
