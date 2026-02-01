7033002b70107218d970295d7a0c52c1f2298b05
mani-hash <manimehalan400@gmail.com>
2026-02-01 13:26:02 +0530

    feat: child access request model [TASK-140]

---
+++ app/Models/ChildAccessRequest.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+
+use Library\Framework\Core\Model;
+
+class ChildAccessRequest extends Model
+{
+    protected static string $table = "children_access_requests";
+
+    protected array $fillable = ["staff_id", "child_id", "reason_title", "reason_description"];
+
+    public function getChild()
+    {
+        $child = Child::find($this->child_id);
+        return $child;
+    }
+
+    public function getStaff()
+    {
+        $staff = Staff::find($this->staff_id);
+        return $staff;
+    }
+}
