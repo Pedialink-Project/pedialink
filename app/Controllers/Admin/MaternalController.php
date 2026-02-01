53b5276e93a8034615b85d787197705b6d27b05a
mani-hash <manimehalan400@gmail.com>
2026-02-01 17:46:38 +0530

    feat: admin maternal overview [TASK-140]

--- app/Controllers/Admin/MaternalController.php
+++ app/Controllers/Admin/MaternalController.php
@@ -2,11 +2,27 @@
 
 namespace App\Controllers\Admin;
 
+use App\Services\Admin\MaternalService;
+use Library\Framework\Http\Request;
+
 class MaternalController
 {
-    public function overview()
+    private MaternalService $maternalService;
+
+    public function __construct()
     {
-        return view("admin/maternal/overview");
+        $this->maternalService = new MaternalService();
+    }
+
+    public function overview(Request $request)
+    {
+        $search = $request->query("search") ?? "";
+        [$maternal, $links] = $this->maternalService->getMaternalData($search);
+
+        return view("admin/maternal/overview", [
+            "maternal" => $maternal,
+            "links" => $links
+        ]);
     }
 
     public function accessRequests()
