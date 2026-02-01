cac16421b67dc7c3b5e155ed04a55adec9d37a96
mani-hash <manimehalan400@gmail.com>
2026-02-01 20:34:51 +0530

    feat: validation vaccine methods [TASK-144]

--- app/Services/Admin/VaccineService.php
+++ app/Services/Admin/VaccineService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Admin;
 
+use App\Helpers\Validator;
 use App\Models\Vaccine;
 
 class VaccineService
@@ -27,5 +28,50 @@ class VaccineService
         $links = array_diff_key($vaccines, ['items' => true]);
 
         return [$resources, $links];
+    }
+
+    private function validateVaccineName(string $name)
+    {
+        $error = null;
+        if (!Validator::validateFieldExistence($name)) {
+            $error = "Vaccine name cannot be empty";
+            return $error;
+        }
+
+        if (!Validator::validateFieldMaxLength($name, 100)) {
+            $error = "Vaccine name cannot be greater than 100 characters";
+            return $error;
+        }
+
+        return $error;
+    }
+
+    private function validateVaccineCode(string $code)
+    {
+        $error = null;
+        if (!Validator::validateFieldExistence($code)) {
+            $error = "Vaccine code cannot be empty";
+            return $error;
+        }
+
+        # NOTE: Add validation for unique code later
+        return $error;
+    }
+
+    public function validateVaccineData(string $name, string $code)
+    {
+        $errors = [];
+
+        $nameError = $this->validateVaccineName($name);
+        if ($nameError) {
+            $errors["name"] = $nameError;
+        }
+
+        $codeError = $this->validateVaccineCode($code);
+        if ($codeError) {
+            $errors["code"] = $codeError;
+        }
+
+        return $errors;
     }
 }
