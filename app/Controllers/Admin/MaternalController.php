f47e11baa5b4f49589b9edcad31bc79a240e8e54
mani-hash <manimehalan400@gmail.com>
2026-02-01 18:22:59 +0530

    feat: maternal access request approve and deny methods [TASK-140]

--- app/Controllers/Admin/MaternalController.php
+++ app/Controllers/Admin/MaternalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers\Admin;
 
+use App\Models\MaternalAccessRequest;
 use App\Services\Admin\MaternalService;
 use Library\Framework\Http\Request;
 
@@ -33,5 +34,35 @@ class MaternalController
             "accessRequests" => $accessRequests,
             "links" => $links
         ]);
+    }
+
+    public function approveAccessRequest(Request $request, int $id)
+    {
+        /** @var MaternalAccessRequest */
+        $accessRequest = MaternalAccessRequest::find($id);
+        
+        $accessRequest->accepted = 1;
+        $accessRequest->save();
+
+        return redirect(route("admin.maternal.access.requests"))
+            ->withMessage(
+                "Access approval granted successfully", 
+                "Success", 
+                "success"
+            );
+    }
+
+    public function denyAccessRequest(Request $request, int $id)
+    {
+        $accessRequest = MaternalAccessRequest::find($id);
+        
+        $accessRequest->delete();
+
+        return redirect(route("admin.maternal.access.requests"))
+            ->withMessage(
+                "Access approval rejected successfully",
+                "Success",
+                "success"
+            );
     }
 }
