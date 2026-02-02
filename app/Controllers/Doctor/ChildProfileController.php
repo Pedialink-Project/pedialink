84c463c57b448bf2b12b7cc265659befec88b661
Keeththigan <keeththi2003@gmail.com>
2026-02-02 22:49:55 +0530

    feat: implement child service integration in ChildProfileController for dynamic children retrieval [TASK-134]

--- app/Controllers/Doctor/ChildProfileController.php
+++ app/Controllers/Doctor/ChildProfileController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controllers\Doctor;
 
+use App\Services\ChildService;
 use Library\Framework\Http\Request;
 
 class ChildProfileController
 {
+    private $childService;
+
+    public function __construct()
+    {
+        $this->childService = new ChildService();
+    }
+
     public function index(Request $request)
     {
-        return view("doctor/childprofile");
+        $staffId = auth()->user()->id;
+        $childern = $this->childService->getChildrenByStaffId($staffId);
+
+        return view("doctor/childprofile",["children"=> $childern]);
     }
 }
