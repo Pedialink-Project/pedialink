95387497afba3e748fe70f897179001fa7b942ee
Keeththigan <keeththi2003@gmail.com>
2026-02-02 23:28:44 +0530

    feat: add configuration for child access reason categories [TASK-134]

---
+++ config/data.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Child acess reason category
+    |--------------------------------------------------------------------------
+    */
+    'accessReason' => ['Medical Consultation',
+    'Vaccination Review',
+    'Growth Monitoring',
+    'Emergency Care',
+    'Follow-up Treatment',],
+
+    
+];
