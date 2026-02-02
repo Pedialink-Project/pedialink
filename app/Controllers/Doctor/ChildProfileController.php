adb1fb4fc7bdd1f602d5aa890c0b2353674d58a9
Keeththigan <keeththi2003@gmail.com>
2026-02-02 23:11:17 +0530

    feat: enhance requestAccess method for child access requests and improve response handling [TASK-134]

--- app/Controllers/Doctor/ChildProfileController.php
+++ app/Controllers/Doctor/ChildProfileController.php
@@ -19,6 +19,32 @@ class ChildProfileController
         $staffId = auth()->user()->id;
         $childern = $this->childService->getChildrenByStaffId($staffId);
 
-        return view("doctor/childprofile",["children"=> $childern]);
+        return view("doctor/childprofile", ["children" => $childern]);
+    }
+
+    public function requestAccess(Request $request)
+    {
+        $staffId = auth()->user()->id;
+        $childId = $request->input("child_id");
+        $reasonTitle = $request->input('reason_title');
+        $reasonDescription = $request->input('reason_description');
+
+
+        $error = $this->childService->requestChildAccess(
+            $staffId,
+            $childId,
+            $reasonTitle,
+            $reasonDescription
+        );
+
+        if ($error) {
+            return redirect(route('doctor.childprofile'))->withMessage ($error, "Request Failed", "info");
+        }
+
+        return redirect(route('doctor.childprofile'))->withMessage(
+            "Access request sent successfully",
+            "Request Sent",
+            "success"
+        );
     }
 }
