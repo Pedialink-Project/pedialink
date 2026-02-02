e4c0f082173f3b4cc056dfacb9d49ddfa062c113
ChamodHaritha <chamodharitha2002@gmail.com>
2026-02-02 20:08:29 +0530

    feat: update route [TASK-134]

--- routes/sectors/doctor.php
+++ routes/sectors/doctor.php
@@ -5,6 +5,7 @@ use App\Controllers\Doctor\DashboardController;
 use App\Controllers\Doctor\ChildProfileController;
 use App\Controllers\Doctor\MaternalProfileController;
 use App\Controllers\Doctor\MaternalHealthController;
+use App\Controllers\Doctor\maternalhealthrecordController;
 use App\Controllers\Doctor\AppointmentController;
 use App\Controllers\NotificationController;
 use App\Controllers\SettingController;
@@ -15,10 +16,10 @@ return [
     ['GET', '/doctor/child-profiles/{id}/health-records', [ChildHealthController::class, 'index'], 'doctor.child.health', ['doctor']],
     ['GET', '/doctor/child-profiles/{id}/vaccination-records', [ChildHealthController::class, 'vaccinationIndex'], 'doctor.child.vaccination', ['doctor']],
     ['GET', '/doctor/maternal-profiles', [MaternalProfileController::class, 'index'], 'doctor.maternal.profiles', ['doctor']],
-    ['GET', '/doctor/maternal-profiles/{id}/health-records', [MaternalHealthController::class, 'index'], 'doctor.maternal.health', ['doctor']],
-    ['POST', '/doctor/maternal-profiles/{id}/health-records/add', [MaternalHealthController::class, 'createMaternalRecord'], 'doctor.maternal.health.add', ['doctor']],
-    ['POST', '/doctor/maternal-profiles/{id}/health-records/{recordId}/edit', [MaternalHealthController::class, 'editMaternalRecord'], 'doctor.maternal.health.edit', ['doctor']],
-    ['POST', '/doctor/maternal-profiles/{id}/health-records/{recordId}/delete', [MaternalHealthController::class, 'deleteMaternalRecord'], 'doctor.maternal.health.delete', ['doctor']],
+    ['GET', '/doctor/maternal-profiles/{id}/health-records', [maternalhealthrecordController::class, 'index'], 'doctor.maternal.health', ['doctor']],
+    ['POST', '/doctor/maternal-profiles/{id}/health-records/add', [maternalhealthrecordController::class, 'createMaternalRecord'], 'doctor.maternal.health.add', ['doctor']],
+    ['POST', '/doctor/maternal-profiles/{id}/health-records/{recordId}/edit', [maternalhealthrecordController::class, 'editMaternalRecord'], 'doctor.maternal.health.edit', ['doctor']],
+    ['POST', '/doctor/maternal-profiles/{id}/health-records/{recordId}/delete', [maternalhealthrecordController::class, 'deleteMaternalRecord'], 'doctor.maternal.health.delete', ['doctor']],
     ['GET', '/doctor/appointments', [AppointmentController::class, 'index'], 'doctor.appointments', ['doctor']],
     ['GET', '/doctor/notification', [NotificationController::class, 'index'], 'doctor.notification', ['doctor']],
     ['GET', '/doctor/settings', [SettingController::class, 'index'], 'doctor.settings', ['doctor']],
