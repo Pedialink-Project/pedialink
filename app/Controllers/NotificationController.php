46e9991885c902cf6ea8c4bd70ceb73559edc9c8
Keeththigan <keeththi2003@gmail.com>
2026-02-02 14:38:21 +0530

    feat: implement markAsRead method in NotificationController to handle notification read status [TASK-142]

--- app/Controllers/NotificationController.php
+++ app/Controllers/NotificationController.php
@@ -17,4 +17,18 @@ class NotificationController
         $notifications = $this->notificationService->getUserNotifications($user);
         return view("auth/notification",["notifications"=> $notifications]);
     }
+
+    public function markAsRead($request, $id)
+    {
+        $user = auth()->user()->id;
+        $error = $this->notificationService->markAsRead($id, $user);
+
+        $userRole = auth()->user()->role;
+
+        if ($error) {
+            return redirect(route("{$userRole}.notification"))->withMessage($error, 'Notification not marked as read.', 'error');
+        }
+
+            return redirect(route("{$userRole}.notification"))->withMessage('success', 'Notification marked as read.', 'success');
+    }
 }
