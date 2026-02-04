4b517c38302063cc1a59c21d3046df6477b3add8
mani-hash <manimehalan400@gmail.com>
2026-02-04 21:14:12 +0530

    feat: add vaccination to schedule route [TASK-144]

--- routes/sectors/admin.php
+++ routes/sectors/admin.php
@@ -57,6 +57,7 @@ return [
     ['POST', '/admin/vaccination/schedule/{id}/enable', [VaccineController::class, 'enableSchedule'], 'admin.vaccination.schedule.enable', ['admin', 'verified']],
     ['POST', '/admin/vaccination/schedule/{id}/disable', [VaccineController::class, 'disableSchedule'], 'admin.vaccination.schedule.disable', ['admin', 'verified']],
     ['GET', '/admin/vaccination/schedule/{schedule_id}/manage', [VaccineController::class, 'manageSchedule'], 'admin.vaccination.schedule.manage', ['admin', 'verified']],
+    ['POST', '/admin/vaccination/schedule/{schedule_id}/manage/add', [VaccineController::class, 'addVaccineToSchedule'], 'admin.vaccination.schedule.manage.add', ['admin', 'verified']],
 
     // Admin appointment
     ['GET', '/admin/appointment', [AppointmentController::class, 'index'], 'admin.appointment', ['admin', 'verified']],
