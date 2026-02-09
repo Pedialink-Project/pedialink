3566212657c763f7d9dbcfe5eb7ab9cde6d5a219
mani-hash <manimehalan400@gmail.com>
2026-02-09 22:14:29 +0530

    feat: integrate dashboard service [TASK-151]

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -2,10 +2,33 @@
 
 namespace App\Controllers\Admin;
 
+use App\Services\Admin\DashboardService;
+
 class DashboardController
 {
+    private DashboardService $dashboardService;
+
+    public function __construct()
+    {
+        $this->dashboardService = new DashboardService();
+    }
+    
     public function index()
     {
-        return view("admin/dashboard");
+        $childrenCount = $this->dashboardService->getTotalChildrenCount();
+        $phmCount = $this->dashboardService->getActivePhmCount();
+        $parentsCount = $this->dashboardService->getTotalParentsCount();
+        $accessRequestsCount = $this->dashboardService->getTotalAccessRequestsCount();
+        $linkageRequestsCount = $this->dashboardService->getTotalLinkageRequestsCount();
+        $doctorsCount = $this->dashboardService->getActiveDoctorsCount();
+
+        return view("admin/dashboard", [
+            "childrenCount" => $childrenCount,
+            "phmCount" => $phmCount,
+            "parentsCount" => $parentsCount,
+            "accessRequestsCount" => $accessRequestsCount,
+            "linkageRequestsCount" => $linkageRequestsCount,
+            "doctorsCount" => $doctorsCount
+        ]);
     }
 }
