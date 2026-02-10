95980615e2f80b7fe8021cc91e779264a02a9fa8
mani-hash <manimehalan400@gmail.com>
2026-02-10 17:48:44 +0530

    feat: admin middleware [TASK-151]

---
+++ app/Middleware/Admin/UserAdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Middleware\Admin;
+
+use App\Middleware\AdminMiddleware;
+use App\Models\Admin;
+use Library\Framework\Http\Request;
+
+class UserAdminMiddleware extends AdminMiddleware
+{
+    public function handle(Request $request, callable $next, array $params)
+    {
+        if (auth()->check() && auth()->user()->isAdmin()) {
+            $adminType = Admin::find(auth()->user()->id)
+                ->getAdminType();
+
+            // allow bypass for super admin while primary access
+            // is for user admin
+            if ($adminType === "super" || $adminType === "user") {
+                return $next($request, $params);
+            }
+        }
+        return view('error/404'); // temporary fallback for errors (Need to implement proper Error class!)
+    }
+}
