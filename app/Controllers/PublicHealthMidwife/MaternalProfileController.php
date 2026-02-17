ec48b6a250534290dd3ef03252ca0c17076358f9
Keeththigan <keeththi2003@gmail.com>
2026-02-18 00:22:27 +0530

    refactor: reorganize index method and initialize maternal service in constructor [TASK-153]

--- app/Controllers/PublicHealthMidwife/MaternalProfileController.php
+++ app/Controllers/PublicHealthMidwife/MaternalProfileController.php
@@ -2,44 +2,50 @@
 
 namespace App\Controllers\PublicHealthMidwife;
 
+use App\Services\MaternalService;
 use Library\Framework\Http\Request;
 
 class MaternalProfileController
 {
+
+    private $maternalService;
+    public function __construct()
+    {
+        $this->maternalService = new MaternalService();
+    }
     public function index(Request $request)
     {
-
-
-        return view("phm/maternalprofiles");
-    }
-
-    public function createMaternal(Request $request)
-    {
         $phmId = auth()->user()->id;
-        $parentId = $request->input("parent_id");
-        $height = $request->input("height");
-        $bloodType = $request->input("blood_type");
-        $lmp = $request->input("lmp");
-
-
-
-
-        // // Create Maternal Profile and Pregnancy Record
-        // try {
-        //     $maternalService = new \App\Services\MaternalService();
-        //     $maternalService->createMaternalProfile(
-        //         $parentId,
-        //         $phmId,
-        //         (float)$height,
-        //         $bloodType,
-        //         $lmp,
-        //         // (int)$gravida, --- IGNORE ---
-        //         // (int)$para --- IGNORE ---
-        //     );
-
-        //     return response()->json(['message' => 'Maternal profile created successfully.']);
-        // } catch (\Exception $e) {
-        //     return response()->json(['error' => 'Failed to create maternal profile: ' . $e->getMessage()], 500);
-        // }
+        $unMaternalProfiles = $this->maternalService->getParentsWithoutMaternal($phmId);
+        return view("phm/maternalprofiles", ['unMaternalProfiles' => $unMaternalProfiles]);
     }
+
+    // public function createMaternal(Request $request)
+
+    //     // $parentId = $request->input("parent_id");
+    //     // $height = $request->input("height");
+    //     // $bloodType = $request->input("blood_type");
+    //     // $lmp = $request->input("lmp");
+
+
+
+
+    //     // // Create Maternal Profile and Pregnancy Record
+    //     // try {
+    //     //     $maternalService = new \App\Services\MaternalService();
+    //     //     $maternalService->createMaternalProfile(
+    //     //         $parentId,
+    //     //         $phmId,
+    //     //         (float)$height,
+    //     //         $bloodType,
+    //     //         $lmp,
+    //     //         // (int)$gravida, --- IGNORE ---
+    //     //         // (int)$para --- IGNORE ---
+    //     //     );
+
+    //     //     return response()->json(['message' => 'Maternal profile created successfully.']);
+    //     // } catch (\Exception $e) {
+    //     //     return response()->json(['error' => 'Failed to create maternal profile: ' . $e->getMessage()], 500);
+    //     // }
+    // }
 }
