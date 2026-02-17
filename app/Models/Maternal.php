5c6ba6d7482fb01920feda2bfa6f3bee071c8cbd
Keeththigan <keeththi2003@gmail.com>
2026-02-17 21:02:27 +0530

    feat: create Maternal model with fillable attributes and getParent method [TASK-153]

---
+++ app/Models/Maternal.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Library\Framework\Core\Model;
+
+class Maternal extends Model
+{
+    protected static string $table = "maternal";
+    protected array $fillable = ["parent_id","type", "height", "blood_group", "created_at"];
+
+    public function getParent(): object|null
+    {
+        return ParentM::find($this->parent_id);
+    }
+
+    
+}
