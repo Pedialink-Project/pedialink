23467687725af3e13a80161fb4f5c44fc05bf97c
Keeththigan <keeththi2003@gmail.com>
2026-02-18 03:35:59 +0530

    fix: add endpoint to end antenatal profiles in MaternalProfileController [TASK-153]

--- routes/sectors/phm.php
+++ routes/sectors/phm.php
@@ -34,6 +34,7 @@ return [
     // Maternal Profile Routes
     ['GET', '/phm/maternal-profiles', [MaternalProfileController::class, 'index'], 'phm.maternal.profiles', ['phm','verified']],
     ['POST', '/phm/maternal-profile/create', [MaternalProfileController::class, 'createMaternal'], 'phm.maternal.create', ['phm','verified']],
+    ['POST','/phm/maternal-profiles/{id}/end', [MaternalProfileController::class,'endAntenatal'], 'phm.maternal.end', ['phm','verified']],
     
     //Maternal Health Record Routes
     ['GET', '/phm/maternal-profiles/{id}/health-records', [MaternalHealthController::class, 'index'], 'phm.maternal.health', ['phm','verified']],
