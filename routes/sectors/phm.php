ec1e6d8a869a3fff9b137977ea5237e2f590654b
Keeththigan <keeththi2003@gmail.com>
2026-02-18 00:10:55 +0530

    feat: add route for creating maternal profiles in PHM sector [TASK-153]

--- routes/sectors/phm.php
+++ routes/sectors/phm.php
@@ -33,7 +33,8 @@ return [
 
     // Maternal Profile Routes
     ['GET', '/phm/maternal-profiles', [MaternalProfileController::class, 'index'], 'phm.maternal.profiles', ['phm','verified']],
-
+    ['POST', '/phm/maternal-profile/create', [MaternalProfileController::class, 'createMaternal'], 'phm.maternal.create', ['phm','verified']],
+    
     //Maternal Health Record Routes
     ['GET', '/phm/maternal-profiles/{id}/health-records', [MaternalHealthController::class, 'index'], 'phm.maternal.health', ['phm','verified']],
     ['GET', '/phm/child-vaccinations/{id}/records', [ChildHealthController::class, 'vaccinationIndex'], 'phm.child.vaccinations', ['phm','verified']],
