31331e5b933804e191294f49d82f0da72a24981c
Keeththigan <keeththi2003@gmail.com>
2026-02-17 21:02:46 +0530

    fix: update fillable attributes and correct getParent method in MaternalRecord model [TASK-153]

--- app/Models/MaternalRecord.php
+++ app/Models/MaternalRecord.php
@@ -7,13 +7,13 @@ use Library\Framework\Core\Model;
 class MaternalRecord extends Model
 {
     protected static string $table = "maternal_records";
-    protected array $fillable = ["parent_id","staff_id", "visit_date", "trimester", "weight", "height", "bmi", "created_at","blood_pressure","blood_sugar","notes","health_status"];
+    protected array $fillable = ["parent_id","staff_id", "visit_date", "trimester", "weight", "bmi", "created_at","blood_pressure","glucose","hemoglobin","fetal_heart_rate","fundal_height","notes","health_status"];
 
-    public function getMaternal(): object|null
+   
+    public function getParent(): object|null
     {
-        return MaternalRecord::find($this->parent_id);
+        return ParentM::find($this->parent_id);
     }
-
     public function getStaff(): object|null
     {
         return Staff::find($this->staff_id);
