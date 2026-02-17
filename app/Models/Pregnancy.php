2f61927528bcfbdc1c1c7756a3e07ef248195566
Keeththigan <keeththi2003@gmail.com>
2026-02-17 21:02:14 +0530

    feat: add Pregnancy model with fillable attributes and getParent method [TASK-153]

---
+++ app/Models/Pregnancy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Library\Framework\Core\Model;
+
+class Pregnancy extends Model
+{
+    protected static string $table = "pregnancy";
+    protected array $fillable = ["maternal_id","started_at","end_at", "lmp", "edd", "gravida", "para", "delivery_outcome", "created_at"];
+
+    public function getParent(): object|null
+    {
+        return Maternal::find($this->maternal_id);
+    }
+
+    
+}
