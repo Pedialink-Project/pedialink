e50890c04ddb98fd70358605f92cec2faa9f300a
mani-hash <manimehalan400@gmail.com>
2026-02-18 09:05:41 +0530

    feat: edit availabilitry method [TASK-152]

--- app/Controllers/Admin/AppointmentController.php
+++ app/Controllers/Admin/AppointmentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers\Admin;
 
+use App\Models\ClinicWeeklyAvailability;
 use App\Services\Admin\AppointmentService;
 use Library\Framework\Http\Request;
 
@@ -38,6 +39,33 @@ class AppointmentController
         return view("admin/appointment/configure", [
             "clinicWeeklyAvailability" => $clinicWeeklyAvailability
         ]);
+    }
+
+    public function editAvailability(Request $request, int $id)
+    {
+        $data = [
+            "e_start_time" => $request->input("e_start_time", ""),
+            "e_end_time" => $request->input("e_end_time", ""),
+            "e_slot_length_minutes" => $request->input("e_slot_length_minutes", 0)
+        ];
+        
+        $errors = $this->appointmentService->validateAvailabilityData($data, true);
+
+        if (count($errors) > 0) {
+            return redirect(route("admin.appointment.configure"))
+                ->withErrors($errors)
+                ->withInput($data)
+                ->with('edit', $id);
+        }
+
+        $clinicWeeklyAvailability = ClinicWeeklyAvailability::find($id);
+        $clinicWeeklyAvailability->start_time = $data['e_start_time'];
+        $clinicWeeklyAvailability->end_time = $data['e_end_time'];
+        $clinicWeeklyAvailability->slot_length_minutes = $data['e_slot_length_minutes'];
+        $clinicWeeklyAvailability->save();
+
+        return redirect(route("admin.appointment.configure"))
+            ->withMessage("Availability updated successfully.", "Success", "success");
     }
 
 }
