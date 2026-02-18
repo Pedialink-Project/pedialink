7e37d817caa44f986907a23cbde8ca06fc0feeae
mani-hash <manimehalan400@gmail.com>
2026-02-18 10:18:43 +0530

    feat: doctor appointment method [TASK-152]

--- app/Controllers/Doctor/AppointmentController.php
+++ app/Controllers/Doctor/AppointmentController.php
@@ -2,13 +2,28 @@
 
 namespace App\Controllers\Doctor;
 
+use App\Services\Doctor\AppointmentService;
 use Library\Framework\Http\Request;
 
 class AppointmentController
 {
+    private AppointmentService $appointmentService;
+
+    public function __construct()
+    {
+        $this->appointmentService = new AppointmentService();
+    }
+
     public function overview(Request $request)
     {
-        return view("doctor/appointment/overview");
+        $search = $request->query("search", "");
+        $filters = $request->query("filters", []);
+        [$appointments, $links] = $this->appointmentService
+            ->getAppointmentOverviewData($search, $filters);
+        return view("doctor/appointment/overview", [
+            "appointments" => $appointments,
+            "links" => $links
+        ]);
     }
 
     public function configure(Request $request)
