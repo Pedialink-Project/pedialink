d36e8bea1ea38e4fb13974744a64120358688cfe
Keeththigan <keeththi2003@gmail.com>
2026-03-03 15:01:13 +0530

    refactor: implement MaternalHealthController to utilize MaternalRecordService for fetching maternal records [TASK-153]

--- app/Controllers/PublicHealthMidwife/MaternalHealthController.php
+++ app/Controllers/PublicHealthMidwife/MaternalHealthController.php
@@ -2,12 +2,30 @@
 
 namespace App\Controllers\PublicHealthMidwife;
 
+use App\Services\MaternalRecordService;
 use Library\Framework\Http\Request;
 
 class MaternalHealthController
 {
-    public function index(Request $request)
+private $maternalRecordService;
+
+    public function __construct()
     {
-        return view("phm/maternalhealth");
+        $this->maternalRecordService = new MaternalRecordService();
+    }
+    public function index(Request $request, int $id)
+    {
+
+        $search = $request->input("search");
+        $filters = $request->input("filters");
+        [$records, $links] = $this->maternalRecordService->getMaternalRecordsByMaternalId($id, $search, $filters);
+        $name = 'gh';
+
+        return view("phm/maternalhealth", [
+            "id" => $id,
+            'name' => $name,
+            "records" => $records,
+            "links" => $links
+        ]);
     }
 }
