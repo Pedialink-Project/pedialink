dc6c69649c9c41cb9d3f88f0e1fd28f54c875b7d
Keeththigan <keeththi2003@gmail.com>
2026-03-04 22:30:26 +0530

    feat: Enhance VaccinationController to retrieve child vaccinations by parent ID [TASK-155]

--- app/Controllers/Parent/VaccinationController.php
+++ app/Controllers/Parent/VaccinationController.php
@@ -2,12 +2,32 @@
 
 namespace App\Controllers\Parent;
 
+use App\Services\Parent\VaccinationService;
+use Library\Framework\Http\Request;
+
 class VaccinationController
 {
-    public function index()
+
+    private VaccinationService $vaccinationService;
+
+    public function __construct()
     {
-        return view("parent/vaccination");
+        $this->vaccinationService = new VaccinationService();
     }
-   
- 
+    public function index(Request $request)
+    {
+
+        $search = $request->query("search", "");
+        $filters = $request->query("filters", []);
+        $parentId = auth()->user()->id;
+
+        [$vaccinations, $links] = $this->vaccinationService->getChildVaccinationByParentId($parentId, $search, $filters);
+        var_dump($vaccinations);
+
+
+        return view("parent/vaccination", [
+            "vaccinations" => $vaccinations,
+            "links" => $links
+        ]);
+    }
 }
