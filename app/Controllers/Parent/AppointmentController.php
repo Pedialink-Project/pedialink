93ea3b0b6d001efab0af0425059617075e1bda5b
Keeththigan <keeththi2003@gmail.com>
2026-03-04 21:34:45 +0530

    fix: Implement appointment cancellation methods with error handling in AppointmentController [TASK-130]

--- app/Controllers/Parent/AppointmentController.php
+++ app/Controllers/Parent/AppointmentController.php
@@ -21,7 +21,7 @@ class AppointmentController
         $parentId = auth()->user()->id;
 
 
-        [$appointments, $links] = $this->appointmentService->getParentAppointmentByParentId($parentId,$search, $filters);
+        [$appointments, $links] = $this->appointmentService->getParentAppointmentByParentId($parentId, $search, $filters);
         return view("parent/my-appointments", [
             "appointments" => $appointments,
             "links" => $links
@@ -40,5 +40,58 @@ class AppointmentController
             "appointments" => $appointments,
             "links" => $links
         ]);
+    }
+
+    public function cancelMyAppointment(Request $request, $id)
+    {
+        $reason = $request->input("reason");
+
+
+        $errors = $this->appointmentService->validateAppointmentCancel($reason);
+
+        if (count($errors) !== 0) {
+            return redirect(route("parent.appointments.my"))
+                ->withInput([
+                    "reason" => $reason
+                ])
+                ->withErrors($errors)
+                ->with("cancelAppointment", $id);
+        }
+
+        $error = $this->appointmentService->cancelAppointment($id, $reason);
+
+
+        if ($error) {
+            return redirect(route("parent.appointments.my"))
+                ->withMessage($error, "Error", "error");
+        }
+
+        return redirect(route("parent.appointments.my"))
+            ->withMessage("Appointment cancelled successfully.", "Success", "success");
+    }
+
+    public function cancelChildAppointment(Request $request, $id)
+    {
+        $reason = $request->input("reason");
+        $errors = $this->appointmentService->validateAppointmentCancel($reason);
+
+        if (count($errors) !== 0) {
+            return redirect(route("parent.appointments.child"))
+                ->withInput([
+                    "reason" => $reason
+                ])
+                ->withErrors($errors)
+                ->with("cancelAppointment", $id);
+        }
+
+        $error = $this->appointmentService->cancelAppointment($id, $reason);
+
+        if ($error) {
+            return redirect(route("parent.appointments.child"))
+                ->withMessage($error, "Error", "error");
+        }
+
+        return redirect(route("parent.appointments.child"))
+            ->withMessage("Appointment cancelled successfully.", "Success", "success");
     }
 }
