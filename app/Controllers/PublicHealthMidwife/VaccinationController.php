8990526dd9d3c415a2f723adbd4d00e86aa1774d
mani-hash <manimehalan400@gmail.com>
2026-03-04 21:36:24 +0530

    feat: mark as completed logic [TASK-154]

--- app/Controllers/PublicHealthMidwife/VaccinationController.php
+++ app/Controllers/PublicHealthMidwife/VaccinationController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers\PublicHealthMidwife;
 
 use App\Models\Child;
+use App\Models\Vaccination;
+use App\Models\VaccinationReminder;
 use App\Services\PublicHealthMidwife\VaccinationService;
 use Library\Framework\Http\Request;
 
@@ -32,5 +34,47 @@ class VaccinationController
             "vaccinations" => $vaccinations,
             "links" => $links,
         ]);
+    }
+
+    public function markAsCompleted(Request $request, int $id, int $recordId)
+    {
+        $child = Child::find($id);
+        if (!$child) {
+            return redirect(route("phm.child.vaccinations", ["id" => $id]))
+                ->withMessage(
+                    "An error occured while marking the vaccination record as completed. Please try again.",
+                    "Error",
+                    "error"
+                );
+        }
+
+        $vaccinationReminder = VaccinationReminder::find($recordId);
+        if (!$vaccinationReminder || $vaccinationReminder->child_id !== $id) {
+            return redirect(route("phm.child.vaccinations", ["id" => $id]))
+                ->withMessage(
+                    "An error occured while marking the vaccination record as completed. Please try again.",
+                    "Error",
+                    "error"
+                );
+        }
+
+        $vaccination = new Vaccination();
+        $vaccination->child_id = $id;
+        $vaccination->schedule_vaccine_id = $vaccinationReminder->schedule_vaccine_id;
+        $vaccination->administered_at = (new \DateTime())->format("Y-m-d H:i:sP");
+        $vaccination->recorded_at = (new \DateTime())->format("Y-m-d H:i:sP");
+        $success = $vaccination->save();
+
+        if ($success) {
+            $vaccinationReminder->status = "complete";
+            $vaccinationReminder->save();
+        }
+
+        return redirect(route("phm.child.vaccinations", ["id" => $id]))
+            ->withMessage(
+                "Vaccination record marked as completed successfully.",
+                "Success",
+                "success"
+            );
     }
 }
