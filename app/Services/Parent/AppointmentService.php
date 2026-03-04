4ffad81217a3a30af647fe722a9813215cd91562
Keeththigan <keeththi2003@gmail.com>
2026-03-04 15:00:01 +0530

    fix: Add AppointmentService to retrieve appointments by child ID with detailed information [TASK-130]

---
+++ app/Services/Parent/AppointmentService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Services\Parent;
+
+use App\Helpers\AppointmentConfigurationHelper;
+use App\Helpers\IntToDayName;
+use App\Helpers\Validator;
+use App\Models\Appointment;
+use App\Models\ClinicWeeklyAvailability;
+use App\Models\ParentChild;
+
+class AppointmentService
+{
+
+
+    public function getAppointmentByChildId(string $search, array $filters = [], $parentId)
+    {
+
+        $childIds = ParentChild::query()->where("parent_id", '=', $parentId)->pluck("child_id");
+        $appointments = Appointment::query()->whereIn('child_id', $childIds);
+
+        if (isset($filters['status'])) {
+            $appointments = $appointments
+                ->whereIn("appointments.status", $filters['status']);
+        }
+
+        $appointments = $appointments
+            ->join("appointment_slots as s", "s.id", "=", "appointments.slot_id")
+            ->orderBy("s.slot_date", "DESC")
+            ->paginate(10)
+            ->toArray();
+
+        $resource = [];
+        foreach ($appointments['items'] as $appointment) {
+            $slot = $appointment->getSlot();
+            $doctor = $slot->getDoctor();
+            $maternal = $appointment->getMaternal();
+            $child = $appointment->getChild();
+            $resource[] = [
+                "id" => $appointment->id,
+                "slot_date" => $slot->slot_date,
+                "start_time" => $slot->start_time,
+                "end_time" => $slot->end_time,
+                "doctor" => $doctor ? [
+                    "id" => $doctor->id,
+                    "name" => $doctor->getUser()->name
+                ] : null,
+                "child" => $child ? [
+                    "id" => $child->id,
+                    "name" => $child->name,
+                ] : null,
+                "maternal" => $maternal ? [
+                    "id" => $maternal->id,
+                    "name" => $maternal->getUser()->name,
+                ] : null,
+                "reason" => $appointment->reason,
+                "notes" => $appointment->notes,
+                "status" => $appointment->status
+            ];
+        }
+
+        $links = array_diff_key($appointments, ['items' => true]);
+
+        return [
+            $resource,
+            $links
+        ];
+    }
+
+    
+
+}
