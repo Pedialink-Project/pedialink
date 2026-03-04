bb77734e08a483039b0635b0ad7160d244d14bb5
Keeththigan <keeththi2003@gmail.com>
2026-03-04 15:10:44 +0530

    fix: Implement parent appointment retrieval method in AppointmentService with detailed information [TASK-130]

--- app/Services/Parent/AppointmentService.php
+++ app/Services/Parent/AppointmentService.php
@@ -7,13 +7,14 @@ use App\Helpers\IntToDayName;
 use App\Helpers\Validator;
 use App\Models\Appointment;
 use App\Models\ClinicWeeklyAvailability;
+use App\Models\Maternal;
 use App\Models\ParentChild;
 
 class AppointmentService
 {
 
 
-    public function getAppointmentByChildId(string $search, array $filters = [], $parentId)
+    public function getChildAppointmentByParentId(string $search, array $filters = [], $parentId)
     {
 
         $childIds = ParentChild::query()->where("parent_id", '=', $parentId)->pluck("child_id");
@@ -67,6 +68,58 @@ class AppointmentService
         ];
     }
 
+    public function getParentAppointmentByParentId(string $search, array $filters = [], $parentId)
+    {
+        $maternalIds = Maternal::query()->where("parent_id", '=', $parentId)->pluck("id");
+        $appointments = Appointment::query()->whereIn('maternal_id', $maternalIds);
+
+        if (isset($filters['status'])) {
+            $appointments = $appointments
+                ->whereIn("appointments.status", $filters['status']);
+        }
+
+        $appointments = $appointments
+            ->join("appointment_slots as s", "s.id", "=", "appointments.slot_id")
+            ->orderBy("s.slot_date", "DESC")
+            ->paginate(10)
+            ->toArray();
+
+        $resource = [];
+        foreach ($appointments['items'] as $appointment) {
+            $slot = $appointment->getSlot();
+            $doctor = $slot->getDoctor();
+            $maternal = $appointment->getMaternal();
+            $child = $appointment->getChild();
+            $resource[] = [
+                "id" => $appointment->id,
+                "slot_date" => $slot->slot_date,
+                "start_time" => $slot->start_time,
+                "end_time" => $slot->end_time,
+                "doctor" => $doctor ? [
+                    "id" => $doctor->id,
+                    "name" => $doctor->getUser()->name
+                ] : null,
+                "child" => $child ? [
+                    "id" => $child->id,
+                    "name" => $child->name,
+                ] : null,
+                "maternal" => $maternal ? [
+                    "id" => $maternal->id,
+                    "name" => $maternal->getUser()->name,
+                ] : null,
+                "reason" => $appointment->reason,
+                "notes" => $appointment->notes,
+                "status" => $appointment->status
+            ];
+        }
+
+        $links = array_diff_key($appointments, ['items' => true]);
+
+        return [
+            $resource,
+            $links
+        ];
+    }
     
 
 }
