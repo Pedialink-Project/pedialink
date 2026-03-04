0662240118953488fb01fd0ee9535eb65fd16a03
mani-hash <manimehalan400@gmail.com>
2026-03-04 20:17:39 +0530

    feat: phm vaccination view records [TASK-154]

--- app/Controllers/PublicHealthMidwife/VaccinationController.php
+++ app/Controllers/PublicHealthMidwife/VaccinationController.php
@@ -2,12 +2,40 @@
 
 namespace App\Controllers\PublicHealthMidwife;
 
+use App\Models\Child;
+use App\Services\PublicHealthMidwife\VaccinationService;
 use Library\Framework\Http\Request;
 
 class VaccinationController
 {
+    private VaccinationService $vaccinationService;
+
+    public function __construct()
+    {
+        $this->vaccinationService = new VaccinationService();
+    }
+
     public function index(Request $request)
     {
         return view("phm/vaccination");
+    }
+
+    public function childVaccinationRecords(Request $request, int $id)
+    {
+        $search = $request->query("search", "");
+        $filters = $request->query("filters", []);
+        [$vaccinations, $links] = $this->vaccinationService->fetchVaccinationRecordsByChildId(
+            $id,
+            $search,
+            $filters
+        );
+
+        $child = Child::find($id);
+        return view("phm/vaccinationrecord", [
+            "id" => $id,
+            "name" => $child->name,
+            "vaccinations" => $vaccinations,
+            "links" => $links,
+        ]);
     }
 }
