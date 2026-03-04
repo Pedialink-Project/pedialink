7bdaf15ba378450f2513c369d076dd97499785fa
Keeththigan <keeththi2003@gmail.com>
2026-03-04 19:49:14 +0530

    fix: Remove unused helper imports in AppointmentService for cleaner code [TASK-130]

--- app/Services/Parent/AppointmentService.php
+++ app/Services/Parent/AppointmentService.php
@@ -2,12 +2,8 @@
 
 namespace App\Services\Parent;
 
-use App\Helpers\AppointmentConfigurationHelper;
 use App\Helpers\Calculator;
-use App\Helpers\IntToDayName;
-use App\Helpers\Validator;
 use App\Models\Appointment;
-use App\Models\ClinicWeeklyAvailability;
 use App\Models\Maternal;
 use App\Models\ParentChild;
 
