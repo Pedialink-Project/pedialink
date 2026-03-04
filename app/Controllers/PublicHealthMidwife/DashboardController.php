5fdeb092af07111f577aa1510b50f58d0810e105
mani-hash <manimehalan400@gmail.com>
2026-03-04 22:25:10 +0530

    feat: pass dashbaord data [TASK-154]

--- app/Controllers/PublicHealthMidwife/DashboardController.php
+++ app/Controllers/PublicHealthMidwife/DashboardController.php
@@ -2,12 +2,28 @@
 
 namespace App\Controllers\PublicHealthMidwife;
 
+use App\Services\PublicHealthMidwife\DashboardService;
 use Library\Framework\Http\Request;
 
 class DashboardController
 {
+    private DashboardService $dashboardService;
+
+    public function __construct()
+    {
+        $this->dashboardService = new DashboardService();
+    }
+
     public function index(Request $request)
     {
-        return view("phm/dashboard");
+        $linkedChildrenCount = $this->dashboardService->getLinkedChildrenCount();
+        $appointmentsCount = $this->dashboardService->getAppointmentsCount();
+        $upcomingVaccinationsCount = $this->dashboardService->getUpcomingVaccinationsCount();
+
+        return view("phm/dashboard", [
+            "linkedChildrenCount" => $linkedChildrenCount,
+            "appointmentsCount" => $appointmentsCount,
+            "upcomingVaccinationsCount" => $upcomingVaccinationsCount
+        ]);
     }
 }
