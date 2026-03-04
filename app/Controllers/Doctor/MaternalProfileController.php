bd81479305108fcc1cec55ce9bd8103e76b058d2
Keeththigan <keeththi2003@gmail.com>
2026-03-04 12:46:34 +0530

    fix: Refactor MaternalProfileController to use MaternalService and streamline maternal data retrieval [TASK-146]

--- app/Controllers/Doctor/MaternalProfileController.php
+++ app/Controllers/Doctor/MaternalProfileController.php
@@ -5,51 +5,26 @@ namespace App\Controllers\Doctor;
 use Library\Framework\Http\Request;
 use App\Models\ParentM;
 use App\Models\User;
-use App\Services\maternalrecordService;
+use App\Services\MaternalService;
 
 class MaternalProfileController
 {
-    protected $maternalrecordService;
+    protected $maternalService;
 
     public function __construct()
     {
-        $this->maternalrecordService = new maternalrecordService();
+        $this->maternalService = new MaternalService();
     }
 
     public function index(Request $request)
     {
-        $parents = ParentM::all();
+        $search = $request->input("search");
+        $filters = $request->input("filters");
+        $doctorId = auth()->user()->id;
+        [$maternals, $links] = $this->maternalService->getMaternalByDoctorId($doctorId, $search, $filters);
+        $unaccesedMaternals = $this->maternalService->getUnaccessedMaternalForStaff($doctorId);
+        $accessReasons = config('data.accessReason');
 
-        $items = array_map(function ($parent) {
-            $user = User::find($parent->id);
-            $latestRecord = $this->maternalrecordService->getLatestMaternalRecord($parent->id);
-            $area = $parent->getArea();
-
-            // Calculate age from date of birth
-            $age = '-';
-            // $dob = $parent->date_of_birth ?? $user->date_of_birth ?? null;
-            // if ($dob) {
-            //     try {
-            //         $age = calculateAge($dob);
-            //     } catch (\Exception $e) {
-            //         $age = '-';
-            //     }
-            // }
-
-            return [
-                'id' => $parent->id,
-                'name' => $user->name ?? 'N/A',
-                'age' => $age,
-                'address' => $parent->address ?? '-',
-                'type' => $parent->type ?? '-',
-                'gs_devision' => $area->code ?? '-',
-                'nic' => $parent->nic ?? ($user->nic ?? '-'),
-                'latest_health_record' => $latestRecord,
-            ];
-        }, $parents ?? []);
-
-        return view("doctor/maternalprofile", [
-            'items' => $items,
-        ]);
+        return view("doctor/maternalprofile", ['maternals' => $maternals, 'links' => $links, 'unacessedMaternals' => $unaccesedMaternals, 'accessReasons' => $accessReasons]);
     }
 }
