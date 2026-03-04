93b68ea651a6585551efb90e5a129aa652085431
Keeththigan <keeththi2003@gmail.com>
2026-03-04 15:10:36 +0530

    fix: Refactor appointment retrieval methods in AppointmentController for improved functionality [TASK-130]

--- app/Controllers/Parent/AppointmentController.php
+++ app/Controllers/Parent/AppointmentController.php
@@ -14,22 +14,28 @@ class AppointmentController
     {
         $this->appointmentService = new AppointmentService();
     }
-    public function myAppointments()
+    public function myAppointments(Request $request)
     {
+        $search = $request->query("search", "");
+        $filters = $request->query("filters", []);
+        $parentId = auth()->user()->id;
 
 
-
-        return view("parent/my-appointments");
+        [$appointments, $links] = $this->appointmentService->getParentAppointmentByParentId($search, $filters, $parentId);
+        return view("parent/my-appointments", [
+            "appointments" => $appointments,
+            "links" => $links
+        ]);
     }
 
     public function childAppointments(Request $request)
     {
-        $search = $request->query("search" , "");
-        $filters = $request->query("filters",[]);
+        $search = $request->query("search", "");
+        $filters = $request->query("filters", []);
         $parentId = auth()->user()->id;
 
 
-        [$appointments, $links] = $this->appointmentService->getAppointmentByChildId($search, $filters, $parentId);
+        [$appointments, $links] = $this->appointmentService->getChildAppointmentByParentId($search, $filters, $parentId);
         return view("parent/child-appointments", [
             "appointments" => $appointments,
             "links" => $links
