e073aabf8bcedd2d6c9bd99dfb80b97b21acf7a5
mani-hash <manimehalan400@gmail.com>
2026-03-05 09:04:06 +0530

    feat: pass base card data [TASK-154]

--- app/Controllers/Doctor/DashboardController.php
+++ app/Controllers/Doctor/DashboardController.php
@@ -2,12 +2,27 @@
 
 namespace App\Controllers\Doctor;
 
+use App\Services\Doctor\DashboardService;
 use Library\Framework\Http\Request;
 
 class DashboardController
 {
+    private DashboardService $dashboardService;
+
+    public function __construct()
+    {
+        $this->dashboardService = new DashboardService();
+    }
+
     public function index(Request $request)
     {
-        return view("doctor/dashboard");
+        $patientsCount = $this->dashboardService->getPatientsCount();
+        $appointmentsCount = $this->dashboardService->getAppointmentsCount();
+        $urgentCasesCount = $this->dashboardService->getUrgentCasesCount();
+        return view("doctor/dashboard", [
+            "patientsCount" => $patientsCount,
+            "appointmentsCount" => $appointmentsCount,
+            "urgentCasesCount" => $urgentCasesCount
+        ]);
     }
 }
