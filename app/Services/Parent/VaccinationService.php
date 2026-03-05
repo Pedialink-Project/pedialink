e270b58870b0588c6069c0a5e44793ab5ec3e567
Keeththigan <keeththi2003@gmail.com>
2026-03-05 20:03:47 +0530

    refactor: rename variable for clarity and enhance vaccination data structure [TASK-157]

--- app/Services/Parent/VaccinationService.php
+++ app/Services/Parent/VaccinationService.php
@@ -14,49 +14,55 @@ class VaccinationService
     public function getChildVaccinationByParentId($parentId, string $search, array $filters = [])
     {
         $childIds = ParentChild::query()->where("parent_id", '=', $parentId)->pluck("child_id");
-        $vaccinations = VaccinationReminder::query()->whereIn('child_id', $childIds);
+        $remainders = VaccinationReminder::query()->whereIn('child_id', $childIds);
 
         if (isset($filters['status'])) {
-            $vaccinations = $vaccinations
+            $remainders = $remainders
                 ->whereIn("status", $filters['status']);
         }
 
-        $vaccinations = $vaccinations
+        $remainders = $remainders   
             ->orderBy("scheduled_date", "DESC")
             ->paginate(10)
             ->toArray();
 
         $resource = [];
-        foreach ($vaccinations['items'] as $vaccination) {
-            $sheduledVaccine = $vaccination->getScheduleVaccine();
-            $vaccine =  $sheduledVaccine->getVaccine() ;
-            $child = $vaccination->getChild();
+        foreach ($remainders['items'] as $remainder) {
+            $sheduledVaccine = $remainder->getScheduleVaccine();
+            $schedule = $sheduledVaccine ? $sheduledVaccine->getSchedule() : null;
+            $vaccine =  $sheduledVaccine->getVaccine();
+            $vaccination = $remainder->getLinkedVaccination();
+            $child = $remainder->getChild();
             $resource[] = [
-                "id" => $vaccination->id,
-                "scheduled_date" => $vaccination->scheduled_date,
+                "id" => $remainder->id,
+                "scheduled_date" => $remainder->scheduled_date,
                 "child" => $child ? [
                     "id" => $child->id,
                     "name" => $child->name,
                 ] : null,
                 "sheduledVaccine" => $sheduledVaccine ? [
                     "dose_number" => $sheduledVaccine->name,
-                    "min_age_days" => $sheduledVaccine->min_age_days,
-                    "due_age_days" => $sheduledVaccine->due_age_days,
-                    "min_age_gap_days" => $sheduledVaccine->min_age_gap_days,
                     "additional_information" => $sheduledVaccine->additional_information,
                 ] : null,
-                "vaccine" =>$vaccine ? [
+                "schedule" => $schedule ? [
+                    "id" => $schedule->id,
+                    "name" => $schedule->name,
+                ] : null,
+                "vaccine" => $vaccine ? [
                     "id" => $vaccine->id,
                     "name" => $vaccine->name,
                     "code" => $vaccine->code,
                 ] : null,
-                "status" => $vaccination->status
+                "status" => $remainder->status,
+                "administered_at" => $vaccination ? $vaccination->administered_at : null,
+                 "recorded_at" => $vaccination ? $vaccination->recorded_at : null,
+
+
             ];
         }
 
-                $links = array_diff_key($vaccinations, ['items' => true]);
+        $links = array_diff_key($remainders, ['items' => true]);
 
-                return [$resource, $links];
-
+        return [$resource, $links];
     }
 }
