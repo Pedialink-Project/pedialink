364d0e8368b68ae6120157ff79eef8356ccbe3b4
Keeththigan <keeththi2003@gmail.com>
2026-03-05 19:00:42 +0530

    feat: integrate GrowthService to fetch children data in GrowthMonitorController [TASK-157]

--- app/Controllers/PublicHealthMidwife/GrowthMonitorController.php
+++ app/Controllers/PublicHealthMidwife/GrowthMonitorController.php
@@ -3,12 +3,22 @@
 namespace App\Controllers\PublicHealthMidwife;
 
 use Library\Framework\Http\Request;
+use App\Services\PublicHealthMidwife\GrowthService;
 
 class GrowthMonitorController
 {
+private $growthService;
+
+    public function __construct()
+    {
+        $this->growthService = new GrowthService();
+    }
+
     public function index(Request $request)
     {
-        return view("phm/growthmonitoring");
+        $children = $this->growthService->getChildrenByPhmId(auth()->id());
+        return view("phm/growthmonitoring",['children' => $children]);
+
     }
 
     public function childGrowthIndex(Request $request, int $id)
