21f570a34212734e90c46166869edde484ca50fc
Keeththigan <keeththi2003@gmail.com>
2026-03-05 20:30:23 +0530

    fix: add 'verified' role to child and vaccination routes for improved access control [TASk-155]

--- routes/sectors/parent.php
+++ routes/sectors/parent.php
@@ -14,25 +14,25 @@ return [
     ['GET', '/parent/dashboard', [DashboardController::class, 'index'], 'parent.dashboard', ['parent', 'verified']],
 
     //Child routes
-    ['GET', '/parent/my-children', [MyChildrenController::class, 'index'], 'parent.my.children', ['parent']],
-    ['GET', '/parent/my-children/{id}', [MyChildrenController::class, 'viewChildDetails'], 'parent.child.details', ['parent']],
+    ['GET', '/parent/my-children', [MyChildrenController::class, 'index'], 'parent.my.children', ['parent','verified']],
+    ['GET', '/parent/my-children/{id}', [MyChildrenController::class, 'viewChildDetails'], 'parent.child.details', ['parent','verified' ]],
 
     //Vaccination routes
-    ['GET', '/parent/vaccination', [VaccinationController::class, 'index'], 'parent.vaccination', ['parent']],
+    ['GET', '/parent/vaccination', [VaccinationController::class, 'index'], 'parent.vaccination', ['parent','verified']],
 
     //Growth tracking routes
-    ['GET', '/parent/growth-tracking', [GrowthController::class, 'index'], 'parent.growth.tracking', ['parent']],
+    ['GET', '/parent/growth-tracking', [GrowthController::class, 'index'], 'parent.growth.tracking', ['parent','verified']],
 
     //Appointment routes
-    ['GET', '/parent/appointments/my-appointments', [AppointmentController::class, 'myAppointments'], 'parent.appointments.my', ['parent']],
-    ['GET', '/parent/appointments/child', [AppointmentController::class, 'childAppointments'], 'parent.appointments.child', ['parent']],
-    ['POST', '/parent/appointment/{id}/cancel', [AppointmentController::class, 'cancelMyAppointment'], 'parent.appointment.cancel', ['parent']],
-    ['POST', '/parent/appointment/{id}/child/cancel', [AppointmentController::class, 'cancelChildAppointment'], 'parent.appointment.child.cancel', ['parent']],
+    ['GET', '/parent/appointments/my-appointments', [AppointmentController::class, 'myAppointments'], 'parent.appointments.my', ['parent','verified']],
+    ['GET', '/parent/appointments/child', [AppointmentController::class, 'childAppointments'], 'parent.appointments.child', ['parent','verified']],
+    ['POST', '/parent/appointment/{id}/cancel', [AppointmentController::class, 'cancelMyAppointment'], 'parent.appointment.cancel', ['parent','verified']],
+    ['POST', '/parent/appointment/{id}/child/cancel', [AppointmentController::class, 'cancelChildAppointment'], 'parent.appointment.child.cancel', ['parent','verified']],
     
     //Event routes
-    ['GET', '/parent/events-campaigns', [EventController::class, 'index'], 'parent.events.campaigns', ['parent']],
-    ['POST', '/parent/events-campaigns/{id}/book', [EventController::class, 'bookEvent'], 'parent.events.campaigns.book', ['parent']],
-    ['POST', '/parent/events-campaigns/{id}/cancel', [EventController::class, 'cancelEventBooking'], 'parent.events.campaigns.cancel', ['parent']],
+    ['GET', '/parent/events-campaigns', [EventController::class, 'index'], 'parent.events.campaigns', ['parent','verified']],
+    ['POST', '/parent/events-campaigns/{id}/book', [EventController::class, 'bookEvent'], 'parent.events.campaigns.book', ['parent','verified']],
+    ['POST', '/parent/events-campaigns/{id}/cancel', [EventController::class, 'cancelEventBooking'], 'parent.events.campaigns.cancel', ['parent','verified']],
 
     //Other routes
     ['GET', '/parent/notification', [NotificationController::class, 'index'], 'parent.notification', ['parent','verified']],
