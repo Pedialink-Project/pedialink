ba3520fac064c51f6a1b2b75851622240b7cac00
Keeththigan <keeththi2003@gmail.com>
2026-03-05 19:00:33 +0530

    feat: implement GrowthService to retrieve child growth data and access requests [TASK-157]

---
+++ app/Services/PublicHealthMidwife/GrowthService.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Services\PublicHealthMidwife;
+
+
+use Library\Framework\Database\QueryBuilder;
+use App\Models\ChildAccessRequest;
+use App\Models\Child;
+
+class GrowthService
+{
+    public function getGrowthData($parentId)
+    {
+        $sql = "
+        SELECT 
+            c.id AS child_id,
+            c.name AS child_name,
+            h.visit_date,
+            h.height,
+            h.weight,
+            h.bmi
+        FROM children c
+        JOIN parent_children pc ON pc.child_id = c.id
+        JOIN child_records h ON h.child_id = c.id
+        WHERE pc.parent_id = :parentId
+        ORDER BY h.visit_date
+        ";
+
+        $rows = QueryBuilder::rawGet($sql, [
+            ':parentId' => $parentId
+        ]);
+
+        $children = [];
+
+        foreach ($rows as $row) {
+
+            $childId = $row['child_id'];
+
+            if (!isset($children[$childId])) {
+                $children[$childId] = [
+                    'name' => $row['child_name'],
+                    'id' => $childId,
+                    'bmi' => [],
+                    'height' => [],
+                    'weight' => [],
+                    'labels' => []
+                ];
+            }
+
+            $children[$childId]['labels'][] = date("M", strtotime($row['visit_date']));
+            $children[$childId]['bmi'][] = (float)$row['bmi'];
+            $children[$childId]['height'][] = (float)$row['height'];
+            $children[$childId]['weight'][] = (float)$row['weight'];
+        }
+
+        return array_values($children);
+    }
+
+    public function getChildrenByPhmId( int $phmId,) {
+
+        $requests = ChildAccessRequest::query()
+            ->where('staff_id', '=', $phmId)
+            ->where('accepted', '=', true)
+            ->get();
+
+        foreach ($requests as $child) {
+
+
+            $childData = $child->getChild();
+            $resource[] = [
+                'id' => $childData->id,
+                'name' => $childData->name,
+            ];
+        }
+        return $resource;
+    }
+}
