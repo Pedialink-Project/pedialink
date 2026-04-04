3074db30ae6118e488a261f1ebb93859966f81ab
Keeththigan <keeththi2003@gmail.com>
2026-04-04 20:00:18 +0530

    refactor: remove sorting logic from getParentCalendarEvents method [TASK-158]

--- app/Services/Parent/CalendarService.php
+++ app/Services/Parent/CalendarService.php
@@ -19,13 +19,6 @@ class CalendarService
         $events = array_merge($events, $this->getVaccinationEvents($parentId));
         $events = array_merge($events, $this->getCampaignEvents());
 
-        usort($events, function ($a, $b) {
-            if ($a['date'] === $b['date']) {
-                return strcmp($a['title'], $b['title']);
-            }
-
-            return strcmp($a['date'], $b['date']);
-        });
 
         return $events;
     }
