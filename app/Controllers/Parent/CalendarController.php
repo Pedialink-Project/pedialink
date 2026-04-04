5594e65e85c1273cd4c39025fd55a19b94886b3f
Keeththigan <keeththi2003@gmail.com>
2026-04-04 19:55:42 +0530

    feat: implement calendar event retrieval for parent view [TASK-158]

--- app/Controllers/Parent/CalendarController.php
+++ app/Controllers/Parent/CalendarController.php
@@ -2,13 +2,25 @@
 
 namespace App\Controllers\Parent;
 
+use App\Services\Parent\CalendarService;
 use Library\Framework\Http\Request;
 
 class CalendarController
 {
+    private CalendarService $calendarService;
+
+    public function __construct()
+    {
+        $this->calendarService = new CalendarService();
+    }
 
     public function index(Request $request)
     {
-        return view("parent/calendar");
+        $parentId = auth()->user()->id;
+        $events = $this->calendarService->getParentCalendarEvents($parentId);
+
+        return view("parent/calendar", [
+            "events" => $events
+        ]);
     }
 }
