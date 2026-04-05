3aa5b31b36ee4d568c38ac32f6786b3babd5c855
Keeththigan <keeththi2003@gmail.com>
2026-04-05 13:26:47 +0530

    fix: remove recent vaccinations modal from child and maternal profiles [TASK-161]

--- resources/views/pages/phm/maternalprofiles.view.php
+++ resources/views/pages/phm/maternalprofiles.view.php
@@ -193,12 +193,7 @@ Maternal Profiles - Overview
                                     </c-modal.viewlist>
 
 
-                                    <c-modal.viewlist title="Recent Vaccinations">
-                                        <c-slot name="list">
-                                            <li>BCG - Dose 1 at 13th of July 2023</li>
-                                            <li>BCG - Dose 2 at 28th of September 2023</li>
-                                        </c-slot>
-                                    </c-modal.viewlist>
+                                    <
 
                                     <c-slot name="close">
                                         Close
