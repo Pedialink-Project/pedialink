e9e256a3c6747ef1e4beff377b3d8110eb4a9f69
Keeththigan <keeththi2003@gmail.com>
2026-04-05 13:37:47 +0530

    fix: handle null values for child medical records in profiles [TASK-161]

--- resources/views/pages/doctor/childprofile.view.php
+++ resources/views/pages/doctor/childprofile.view.php
@@ -163,10 +163,10 @@ Child Profiles
                                         </c-slot>
                                         @else
                                         <c-slot name="list">
-                                            <li>Height:{{ $child['record']['height'] }} cm</li>
-                                            <li>Weight: {{ $child['record']['weight'] }}  kg</li>
-                                            <li>BMI Value: {{ $child['record']['bmi'] }}</li>
-                                            <li>Head circumference: {{ $child['record']['head_circumference'] }} cm</li>
+                                                <li>Height: {{ $child['record']['height'] ?? '' }} cm</li>
+                                                <li>Weight: {{ $child['record']['weight'] ?? '' }} kg</li>
+                                                <li>BMI Value: {{ $child['record']['bmi'] ?? '' }}</li>
+                                                <li>Head circumference: {{ $child['record']['head_circumference'] ?? '' }} cm</li>
                                         </c-slot>
                                         
                                         
