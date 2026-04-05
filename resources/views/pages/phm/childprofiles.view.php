af6cc779289388f48bf5ec6699c39358f6019ad9
Keeththigan <keeththi2003@gmail.com>
2026-04-05 13:20:21 +0530

    fix: update medical records display logic in child profiles view [TASK-161]

--- resources/views/pages/phm/childprofiles.view.php
+++ resources/views/pages/phm/childprofiles.view.php
@@ -220,16 +220,16 @@ PHM Child Profiles
                                     @endif
 
                                     <c-modal.viewlist title="Latest Medical Records">
-                                        @if($child['record'])
-                                        <c-slot name="list">
-                                            <li>Height:{{ $child['record']['height'] }}cm</li>
-                                            <li>Weight: {{ $child['record']['weight'] }}kg</li>
-                                            <li>BMI Value: {{ $child['record']['bmi'] }}</li>
-                                            <li>Head circumference: {{ $child['record']['head_circumference'] }}cm</li>
+                                            @if($child['record'] == NULL)
+                                            <c-slot name="list">
+                                            <li>No medical records found.</li>
                                         </c-slot>
                                         @else
                                         <c-slot name="list">
-                                            <li>No medical records found.</li>
+                                            <li>Height:{{ $child['record']['height'] }} cm</li>
+                                            <li>Weight: {{ $child['record']['weight'] }}  kg</li>
+                                            <li>BMI Value: {{ $child['record']['bmi'] }}</li>
+                                            <li>Head circumference: {{ $child['record']['head_circumference'] }} cm</li>
                                         </c-slot>
                                         @endif
                                     </c-modal.viewlist>
