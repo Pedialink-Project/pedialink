fdf375eb6b50c7ac67db9d39a9b34a3079ffb73e
mani-hash <manimehalan400@gmail.com>
2026-04-11 14:57:14 +0530

    feat: go back button and integrate view vaccination card button [TASK-164]

--- resources/views/pages/doctor/vaccinationrecord.view.php
+++ resources/views/pages/doctor/vaccinationrecord.view.php
@@ -4,6 +4,15 @@
 Doctor Vaccination
 @endsection
 
+@section('back')
+    <c-link href="{{ route('doctor.child.profiles') }}">
+        <c-slot name="icon">
+            <svg width="25px" height="25px" viewBox="0 0 1024 1024" fill="#000000" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z" fill="" /></svg>
+        </c-slot>
+        Go Back
+    </c-link>
+@endsection
+
 @section('header')
 <svg width="28" height="28" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g clip-path="url(#clip0_474_7688)">
@@ -32,7 +41,11 @@ Vaccination Details of {{ $name .' (M-00'.$id.')' }}
 
 @section('content')
 <c-table.controls action="{{ route('phm.child.vaccinations', ['id' => $id]) }}" :filters="['status' => ['complete', 'pending', 'overdue']]">
-
+    <c-slot name="extrabtn">
+        <c-link href="{{ route('vaccination.child.card', ['id' => $id]) }}" type="primary">
+            View Vaccination Card
+        </c-link>
+    </c-slot>
 </c-table.controls>
 <c-table.wrapper card="1">
     <div class="table-wrapper" data-responsive="true">
