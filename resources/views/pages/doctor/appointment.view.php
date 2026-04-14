a4f0b844d6bf6b83ea7a2617c8e4848ec139215b
mani-hash <manimehalan400@gmail.com>
2026-04-14 14:37:29 +0530

    feat: update frontend to include appointment history changes [TASK-164]

--- resources/views/pages/doctor/appointment.view.php
+++ resources/views/pages/doctor/appointment.view.php
@@ -14,7 +14,7 @@
         <path d="M9.16663 10L14.1666 10" stroke="#3A3C41" stroke-width="1.5" stroke-linecap="round"/>
         <path d="M9.16663 14.1667L14.1666 14.1667" stroke="#3A3C41" stroke-width="1.5" stroke-linecap="round"/>
     </svg>
-    Recent Appointments
+    {{ !empty($history) && $history['status'] === true ? "Appointment History - " . $history['name'] : "Recent Appointments" }}
 @endsection
 
 @section('css')
@@ -22,7 +22,15 @@
 @endsection
 
 @section('content')
-    <c-table.controls :filters="['status' => ['confirmed', 'pending', 'attended', 'cancelled', 'no-show']]" action="{{ route('doctor.appointments.overview')}}">
+    <?php
+    $actionRouteName = 'doctor.appointments' . (!empty($history) && $history['status'] === true ? '.history' : '.overview');
+    $actionRoute = route($actionRouteName);
+
+    if (!empty($history) && $history['status'] === true) {
+        $actionRoute = route($actionRouteName, ['id' => $history['id'], 'type' => $history['type']]);
+    }
+    ?>
+    <c-table.controls :filters="['status' => ['confirmed', 'pending', 'attended', 'cancelled', 'no-show']]" action="{{ $actionRoute }}">
         <c-slot name="filter">
             <c-button variant="outline">
                 <img src="{{ asset('assets/icons/filter.svg') }}" />
