dff9d9dceb8ae2d2f58f01d272306b90eabf27a6
Keeththigan <keeththi2003@gmail.com>
2026-04-15 21:52:43 +0530

    fix: add back navigation link to appointment, child health, maternal health, and growth tracking views for improved user experience [TASK-170]

--- resources/views/pages/phm/childhealth.view.php
+++ resources/views/pages/phm/childhealth.view.php
@@ -8,6 +8,17 @@ PHM Child Health Records
 <link rel="stylesheet" href="{{ asset('css/pages/phm/child-health.css') }}">
 @endsection
 
+@section('back')
+	<c-link href="{{ $backUrl ?? 'javascript:history.back()' }}">
+		<c-slot name="icon">
+			<svg width="25px" height="25px" viewBox="0 0 1024 1024" fill="#000000" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg">
+				<path d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z" fill="" />
+			</svg>
+		</c-slot>
+		Go Back
+	</c-link>
+@endsection
+
 @section('header')
 <svg width="28" height="28" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g clip-path="url(#clip0_474_12888)">
