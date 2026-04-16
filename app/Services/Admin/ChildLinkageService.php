ba192f04bd5e66215d2421db00e9cb86dfc2fa38
mani-hash <manimehalan400@gmail.com>
2026-04-16 13:02:15 +0530

    fix: linkage bug [TASK-167]

--- app/Services/Admin/ChildLinkageService.php
+++ app/Services/Admin/ChildLinkageService.php
@@ -13,39 +13,38 @@ class ChildLinkageService
         $data = [];
 
         $childMiscs = ChildMisc::query()
+            ->leftJoin("parents", "parents.nic", "=", "child_miscs.parent_nic")
+            ->whereNotNull("parents.nic")
             ->where("accepted", "=", 0)
             ->orderBy("id", "ASC")
             ->paginate(10)
             ->toArray();
-
-        // var_dump($childMiscs); // Debugging line, can be removed later
 
         foreach ($childMiscs["items"] as $misc) {
             $child = $misc->getChild();
             $parent = $misc->getParent();
             $user = $parent ? $parent->getUser() : null;
 
-            if ($child && $parent) {
-                $data[] = [
-                    "id" => $misc->id,
-                    "child" => [
-                        "id" => $child->id,
-                        "name" => $child->name,
-                        "parent_count" => count($child->getParents() ?? []),
-                        "division" => $child->getArea()?->code,
-                    ],
-                    "parent" => [
-                        "id" => $parent->id,
-                        "name" => $user ? $user->name : null,
-                        "nic" => $parent->nic,
-                        "type" => $parent->type,
-                        "address" => $parent->address,
-                        "created_at" => $user ? $user->created_at : null,
-                        "division" => $parent->getArea()?->code,
-                    ],
-                    "accepted" => $misc->accepted
-                ];
-            }
+            
+            $data[] = [
+                "id" => $misc->id,
+                "child" => [
+                    "id" => $child->id,
+                    "name" => $child->name,
+                    "parent_count" => count($child->getParents() ?? []),
+                    "division" => $child->getArea()?->code,
+                ],
+                "parent" => [
+                    "id" => $parent->id,
+                    "name" => $user ? $user->name : null,
+                    "nic" => $parent->nic,
+                    "type" => $parent->type,
+                    "address" => $parent->address,
+                    "created_at" => $user ? $user->created_at : null,
+                    "division" => $parent->getArea()?->code,
+                ],
+                "accepted" => $misc->accepted
+            ];
         }
 
         $links = array_diff_key($childMiscs, ['items' => true]);
