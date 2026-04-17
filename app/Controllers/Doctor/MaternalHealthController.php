f20add85c31156d3bc2ce51fa3fb9c305aa46497
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-18 01:42:01 +0530

    feat:edit controller  [TASK-171]

--- app/Controllers/Doctor/MaternalHealthController.php
+++ app/Controllers/Doctor/MaternalHealthController.php
@@ -86,4 +86,43 @@ class MaternalHealthController
             ->withMessage("Health record added successfully.", "Success", "success");
     }
 
+    public function editHealthRecord(Request $request, int $id, int $recordId)
+    {
+        $visitDate = $request->input('e_visit_date');
+        $bloodPressure = $request->input('e_blood_pressure');
+        $weight = $request->input('e_weight');
+        $hemoglobin = $request->input('e_hemoglobin');
+        $glucose = $request->input('e_glucose');
+        $fetalHeartRate = $request->input('e_fetal_heart_rate');
+        $fundalHeight = $request->input('e_fundal_height');
+
+        $staffId = auth()->user()->id;
+
+
+        $errors = $this->maternalRecordService->validateMaternalHealthData(
+            $visitDate,
+            $bloodPressure,
+            $weight,
+            $hemoglobin,
+            $glucose,
+            $fetalHeartRate,
+            $fundalHeight,
+            true
+        );
+
+        if (count($errors) > 0) {
+            return redirect(route("doctor.maternal.health", ["id" => $id]))
+                ->withInput([
+                    "e_weight" => $weight,
+                    "e_blood_pressure" => $bloodPressure,
+                    "e_hemoglobin" => $hemoglobin,
+                    "e_glucose" => $glucose,
+                    "e_fetal_heart_rate" => $fetalHeartRate,
+                    "e_fundal_height" => $fundalHeight,
+                    "e_visit_date" => $visitDate,
+                ])
+                ->withErrors($errors)
+                ->with("edit", $recordId);
+        }
+
 }
