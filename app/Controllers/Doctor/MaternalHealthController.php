8388fde1de8206e235887c3388d192b10439d7a2
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-17 16:47:07 +0530

    feat:edit controller  [TASK-171]

--- app/Controllers/Doctor/MaternalHealthController.php
+++ app/Controllers/Doctor/MaternalHealthController.php
@@ -15,4 +15,20 @@ class MaternalHealthController
         $this->maternalRecordService = new MaternalRecordService();
     }
 
+    public function index(Request $request, int $id)
+    {
+
+        $search = $request->input("search");
+        $filters = $request->input("filters");
+        [$records, $links] = $this->maternalRecordService->getMaternalRecordsByMaternalId($id, $search, $filters);
+        $name = $this->maternalRecordService->getMaternalNameByMaternalId($id);
+
+        return view("doctor/maternalhealth", [
+            "id" => $id,
+            'name' => $name,
+            "records" => $records,
+            "links" => $links
+        ]);
+    }
+
 }
