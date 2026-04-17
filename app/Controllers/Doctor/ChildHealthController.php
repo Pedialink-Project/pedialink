ec301c0f5ffa4bae56c1e638e4fe9a5271674804
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-17 16:46:49 +0530

    feat:edit controller  [TASK-171]

--- app/Controllers/Doctor/ChildHealthController.php
+++ app/Controllers/Doctor/ChildHealthController.php
@@ -17,5 +17,12 @@ class ChildHealthController
         $this->childRecordService = new ChildRecordService();
         $this->vaccinationService = new VaccinationService();
     }
+    public function index(Request $request, int $id)
+    {
+
+        $search = $request->input("search");
+        $filters = $request->input("filters");
+        [$records, $links] = $this->childRecordService->getChildRecordsByChildId($id, $search, $filters);
+        $name = $this->childRecordService->getChildNameById($id);
 
 }
