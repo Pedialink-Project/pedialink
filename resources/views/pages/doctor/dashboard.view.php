936489702e29c290711eaca745174b2ba1320726
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-19 15:41:22 +0530

    feat: fix view files [TASK-171]

--- resources/views/pages/doctor/dashboard.view.php
+++ resources/views/pages/doctor/dashboard.view.php
@@ -43,3 +43,46 @@ Doctor Dashboard
         </c-pill>
     </section>
 </div>
+
+<main class="container">
+    <div class="left-col">
+
+
+        <!-- Upcoming Events Card -->
+        <c-card class="card growth-card">
+            <div class="header">
+                <div class="title-section">
+                    <span class="card-title">Patient Case Overview</span>
+                    <span class="card-subtitle">Risk rates of assigned patients</span>
+                </div>
+                <!-- Child Selector -->
+                <c-select name='child' class="select-type" placeholder="Select Type" value="children">
+                    <li id="select-children-chart" class="select-item" data-value="children">Children</li>
+                    <li id="select-mother-chart" class="select-item" data-value="mothers">Mothers</li>
+                </c-select>
+            </div>
+            <hr class="divider">
+            <div class="card-body growth-card">
+                <canvas id="riskChart">
+                </canvas>
+                <canvas id="riskChartMother">
+                </canvas>
+            </div>
+        </c-card>
+
+        <!-- Upcoming Appoinments Card -->
+        <c-card class="card appoinment-card">
+            <div class="header">
+                <div class="title-section">
+                    <span class="card-title">Upcoming Appoinments</span>
+                    <span class="card-subtitle">Your scheduled visits to the clinic</span>
+                </div>
+                <c-link type="secondary" href="{{ route('doctor.appointments.overview') }}">View Schedule</c-link>
+            </div>
+            <hr class="divider">
+            <div class="card-body">
+                @if(count($upcomingAppointments) <= 0)
+                    <c-emptycard
+                        title="No upcoming appointments"
+                        description="There are no scheduled appointments in the near future."
+                    />
