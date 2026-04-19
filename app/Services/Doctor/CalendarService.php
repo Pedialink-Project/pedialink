64136f6560afe4c822c686c0f750dd38bdc7636c
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-19 14:21:48 +0530

    feat: fix doctor's services [TASK-171]

--- app/Services/Doctor/CalendarService.php
+++ app/Services/Doctor/CalendarService.php
@@ -4,3 +4,20 @@ namespace App\Services\Doctor;
 
 use App\Helpers\Calculator;
 use App\Models\Appointment;
+
+class CalendarService
+{
+    public function getDoctorCalendarEvents(int $doctorId): array
+    {
+        return $this->getAppointmentEvents($doctorId);
+    }
+
+    private function getAppointmentEvents(int $doctorId): array
+    {
+        $appointments = Appointment::query()
+            ->join('appointment_slots as s', 's.id', '=', 'appointments.slot_id')
+            ->where('s.doctor_id', '=', $doctorId)
+            ->orderBy('s.slot_date', 'ASC')
+            ->get();
+
+        $events = [];
