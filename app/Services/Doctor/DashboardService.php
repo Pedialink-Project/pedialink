6ecff9f1a66221734737c55f5b704f8c0655cc06
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-19 10:08:59 +0530

    feat: fix doctor's services  [TASK-171]

---
+++ app/Services/Doctor/DashboardService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services\Doctor;
+
+use App\Helpers\Calculator;
+use App\Models\Appointment;
+use App\Models\Child;
+use App\Models\ChildRecord;
+use App\Models\Doctor;
+use App\Models\Maternal;
+use App\Models\MaternalRecord;
+use Library\Framework\Database\QueryBuilder;
