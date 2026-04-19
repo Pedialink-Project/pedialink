b61e9a28025392c1757e0e8952bd1b7af9cc3a7a
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-19 14:28:51 +0530

    feat: fix doctor's services [TASK-171]

--- app/Services/Doctor/AppoinmentService.php
+++ app/Services/Doctor/AppoinmentService.php
@@ -69,3 +69,33 @@ class AppointmentService
             if ($doctor == null || ($doctor && $doctor->id !== auth()->user()?->id)) {
                 continue; // Skip appointments that don't belong to the logged-in doctor
             }
+
+            $maternal = $appointment->getMaternal();
+            $child = $appointment->getChild();
+            $resource[] = [
+                "id" => $appointment->id,
+                "slot_date" => $slot->slot_date,
+                "start_time" => Calculator::formatTimeToAmPm($slot->start_time),
+                "end_time" => Calculator::formatTimeToAmPm($slot->end_time),
+                "child" => $child ? [
+                    "id" => $child->id,
+                    "name" => $child->name,
+                    "division" => $child->getArea()->code,
+                ] : null,
+                "maternal" => $maternal ? [
+                    "id" => $maternal->id,
+                    "name" => $maternal->getUser()->name,
+                    "division" => $maternal->getParent()->getArea()->code,
+                ] : null,
+                "reason" => $appointment->reason,
+                "status" => $appointment->status
+            ];
+        }
+
+        $links = array_diff_key($appointments, ['items' => true]);
+
+        return [
+            $resource,
+            $links
+        ];
+    }
