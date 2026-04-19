92fad1520bcc5eaf6b0f5047769f81bfbd7e93a6
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-19 14:21:37 +0530

    feat: fix doctor's services [TASK-171]

--- app/Services/Doctor/AppoinmentService.php
+++ app/Services/Doctor/AppoinmentService.php
@@ -9,3 +9,15 @@ use App\Helpers\Validator;
 use App\Models\Appointment;
 use App\Models\ClinicWeeklyAvailability;
 use App\Models\DoctorWeeklyAvailability;
+
+class AppointmentService
+{
+    public function getAppointmentOverviewData(
+        string $search,
+        array $filters = [],
+        $history = false,
+        array $info = [
+            'type' => 'child',
+            'id' => null
+        ]
+    )
