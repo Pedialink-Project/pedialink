152a305c5d5814156d59794b8dc1710d3192ce34
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-19 15:22:16 +0530

    feat: fix view files [TASK-171]

--- resources/views/pages/doctor/dashboard.view.php
+++ resources/views/pages/doctor/dashboard.view.php
@@ -13,3 +13,33 @@ Doctor Dashboard
 
 
 @section('content')
+<div class="top-section">
+
+    <section class="greet">
+        <h1>Welcome <span class="user-name">{{ auth()->check() ? auth()->user()->name : 'Parent Name'}}</span>
+        </h1>
+    </section>
+    <section class="pill-container">
+        <c-pill>
+            <c-slot name="title">Assigned Patients</c-slot>
+            <c-slot name="number">{{ $patientsCount }}</c-slot>
+            <c-slot name="icon">
+                <img src="{{asset('assets/icons/baby-01.svg')}}">
+            </c-slot>
+        </c-pill>
+        <c-pill>
+            <c-slot name="title">Appoinments</c-slot>
+            <c-slot name="number">{{ $appointmentsCount }}</c-slot>
+            <c-slot name="icon">
+                <img src="{{asset('assets/icons/profile.svg')}}">
+            </c-slot>
+        </c-pill>
+        <c-pill>
+            <c-slot name="title">Urgent Cases</c-slot>
+            <c-slot name="number">{{ $urgentCasesCount }}</c-slot>
+            <c-slot name="icon">
+                <img src="{{asset('assets/icons/vaccine.svg')}}">
+            </c-slot>
+        </c-pill>
+    </section>
+</div>
