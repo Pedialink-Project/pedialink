95ab4c29e292de967b7c606e77d9f51c609bd7db
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-19 15:18:12 +0530

    feat: fix view files [TASK-171]

---
+++ resources/views/pages/doctor/childprofile.view.php
@@ -0,0 +1,11 @@
+@extends('layout/portal')
+
+@section('title')
+Doctor Child Profiles
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/pages/doctor/child.css') }}">
+@endsection
+
+@section('header')
