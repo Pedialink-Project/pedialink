5d30388a2f16f4c675d2c5a6f8892a13348e4973
ChamodHaritha <chamodharitha2002@gmail.com>
2026-04-19 15:18:19 +0530

    feat: fix view files [TASK-171]

---
+++ resources/views/pages/doctor/dashboard.view.php
@@ -0,0 +1,15 @@
+@extends('layout/portal')
+
+@section('title')
+Doctor Dashboard
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/pages/doctor/dashboard.css') }}">
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+@endsection
+
+
+
+@section('content')
